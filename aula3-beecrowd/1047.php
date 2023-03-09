03739ba476aa98e02490a4d63be5b2393113a595
Davi1544 <davi.ana969@gmail.com>
2023-03-08 22:29:32 -0300

    exe2

--- aula3-beecrowd/1047.php
+++ aula3-beecrowd/1047.php
@@ -1,20 +1,50 @@
 <?php
-
     $linha1 = readline();
 
     $val1 = explode(' ', $linha1);
+    
     $hi = floatval($val1[0]);
     $mi = floatval($val1[1]);
     $hf = floatval($val1[2]);
     $mf = floatval($val1[3]);
     
-    $sumH = 0;
+    $sumH = 20;
     $sumM = 0;
     
-    while($mi != $mf){
-     $mi++;
-     if($mi == 60) $mi = 0;
-     $sumM++;
+    if($mi == $mf && $hi == $hf){
+        $sumM = 0;
+        $sumH = 24;
+    }else{
+        if($hf > $hi){
+          if($mf >= $mi){
+            $sumH = $hf - $hi;
+            $sumM = $mf - $mi;
+          }
+          if($mf < $mi){
+            $sumH = $hf - $hi -1;
+            $sumM = 60 - ($mi - $mf);
+          }
+        }
+        else if($hi > $hf){
+          if($mf >= $mi){
+            $sumH = 24 - ($hi - $hf);
+            $sumM = $mf - $mi;
+          }
+          if($mf < $mi){
+            $sumH = 23 - ($hi - $hf);
+            $sumM = 60 - ($mi - $mf);
+          }
+        }else if($hi == $hf){
+          if($mf >= $mi){
+            $sumH = $hf - $hi;
+            $sumM = $mf - $mi;
+          }
+          if($mf < $mi){
+            $sumH = 23;
+            $sumM = 60 - ($mi - $mf);
+          }
+        }
     }
+        
     print("O JOGO DUROU $sumH HORA(S) E $sumM MINUTO(S)\n");
 ?>
